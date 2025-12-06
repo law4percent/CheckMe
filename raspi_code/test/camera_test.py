1c324377b27928b5e08c85ab3ec5da85ff007b0a
Lawrence Roble <lawrence7roble@gmail.com>
2025-12-07 03:44:44 +0800

    Finallizing 3

--- raspi_code/test/camera_test.py
+++ raspi_code/test/camera_test.py
@@ -1,15 +1,22 @@
 from picamera2 import Picamera2
 import cv2
-import numpy as np
 
+# Initialize camera
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"size": (640, 480)})
+
+config = picam2.create_preview_configuration(
+    main={"size": (640, 480)}
+)
+
 picam2.configure(config)
 picam2.start()
 
+print("Press 'q' to exit...")
+
 while True:
     frame = picam2.capture_array()
-    cv2.imshow("Raspi Camera", frame)
+
+    cv2.imshow("Raspberry Pi Camera", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
