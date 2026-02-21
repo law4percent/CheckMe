ef40d910e654b4be6b2e60432b58180b5d16f460
Lawrence Roble <lawrence7roble@gmail.com>
2026-02-22 05:26:57 +0800

    WIP: Working for scanner

--- raspi_code/main.py
+++ raspi_code/main.py
@@ -4,6 +4,7 @@ from services.keypad_hardware import Keypad3x4
 from services.auth import TeacherAuth
 from services.lcd_hardware import detect_i2c_address, LCD_I2C, LCDSize
 from services.logger import get_logger
+from services.l3210_scanner_hardware import L3210Scanner
 
 
 log = get_logger("main.py")
@@ -20,13 +21,15 @@ def main():
         # Authentication Setup
         auth = TeacherAuth()
     except Exception as e:
-        log(f"Error occur during setup: {e}", "error")
+        log(f"Error occured during setup: {e}", "error")
     
     
     
     lcd.show("Initilaizing...", duration=3)
     
+    # =============================
     # STEP 1: Authenticating
+    # =============================
     if not auth.is_authenticated():
         lcd.show(
             content   = [
@@ -35,9 +38,9 @@ def main():
             ],
             duration  = 2
         )
-        lcd.show("LOGIN REQUIRED", duration=3)
         
         while True:
+            lcd.show("LOGIN REQUIRED", duration=3)
             lcd.show("Enter 8-digit PIN:")
             
             temp_code = keypad.read_input(
@@ -47,25 +50,92 @@ def main():
                 timeout         = 60 * 5
             )
             if temp_code == None:
-                lcd.show("LOGIN REQUIRED", duration=3)
                 continue
             
             success, message = auth.login_with_temp_code(temp_code)
             if not success:
-                lcd.show("LOGIN REQUIRED", duration=3)
                 log(message, log_type="warning")
                 continue
             
             break
         
     user = auth.get_current_user()
-    
-    # 
-    user.username
-    user.teacher_uid
+    # Sample usage for later
+    # user.username
+    # user.teacher_uid
 
 
-
+    # =============================
+    # STEP 2: Home Menu
+    # [1] Scan Answer Key
+    # [2] Check Answer Sheets
+    # [3] Settings
+    # =============================
+    main_menu_options = [
+        "Scan Answer Key",
+        "Check Sheets",
+        "Settings",
+    ]
+    answer_key_menu_options = [
+        "Scan",
+        "Done & Save",
+        "Cancel",
+    ]
+    while True:
+        # =============================
+        # 2.1 Scan Answer Key Procedure
+        # =============================
+        selected_option_from_main_menu = lcd.show_scrollable_menu(
+            title           = "MAIN MENU",
+            options         = main_menu_options,
+            scroll_up_key   = "2",
+            scroll_down_key = "8",
+            select_key      = "*",
+            exit_key        = "#",
+            keypad          = keypad.read_key
+        )
+        if selected_option_from_main_menu == 0:
+            # =============================
+            # 2.1.1 Ask for the total number of questions
+            # =============================
+            
+            exact_total_number_of_questions = keypad.read_input(
+                length      = 1,
+                valid_keys  = ['0','1','2','3','4','5','6','7','8','9'],
+                timeout     = 60 * 5
+            )
+            if exact_total_number_of_questions == None:
+                continue # Back to Main Menu
+            
+            selected_option_from_answer_key_menu = lcd.show_scrollable_menu(
+                title           = "SCAN ANSWER KEY",
+                options         = answer_key_menu_options,
+                scroll_up_key   = "2",
+                scroll_down_key = "8",
+                select_key      = "*",
+                exit_key        = "#",
+                keypad          = keypad.read_key
+            )
+            
+            # =============================
+            # 2.1.1.1 Scan
+            # =============================
+            if selected_option_from_answer_key_menu == 0:
+                pass
+            
+            # =============================
+            # 2.1.1.2 Done & Save
+            # =============================
+            if selected_option_from_answer_key_menu == 1:
+                pass
+            
+            # =============================
+            # 2.1.1.3 Cancel
+            # =============================
+            if selected_option_from_answer_key_menu == 2:
+                pass
+            
+         
 
 def find_i2c_address():
     log("Scanning I2C bus...", log_type="info")
