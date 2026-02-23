80dfa084be8114d1199834326187a4a17bc9ae31
law4percent <lawrence7roble@gmail.com>
2026-02-23 13:47:48 +0800

    Polish and add return args

--- raspi_code/services/scorer.py
+++ raspi_code/services/scorer.py
@@ -1,6 +1,11 @@
+from .logger import get_logger
+
+log = get_logger("scorer.py")
+
 def compare_answers(
     student_answers: dict,
-    answer_key_data: dict
+    answer_key_data: dict,
+    lcd: any
 ) -> tuple:
     """
     Compare student answers against the answer key.
@@ -8,22 +13,46 @@ def compare_answers(
     Returns:
         (score, total_questions, breakdown_dict)
     """
-    answer_key = answer_key_data.get("answer_key", {})
-    total = len(answer_key)
-    score = 0
-    breakdown = {}
+    answer_key      = answer_key_data.get("answer_key", {})
+    total           = int(answer_key_data.get("total_questions", 0))
+    score           = 0
+    breakdown       = {}
+    is_final_score  = True
+
+    total_answer_keys = len(answer_key)
+
+    if total != total_answer_keys:
+        log(
+            "\nTotal questions is not same to quantity to total answer_key", 
+            f"\ntotal_questions: {total}",
+            f"\total_answer_keys: {total_answer_keys}",
+            log_type="warning"
+        )
+        lcd.show([
+            f"{total} != {total_answer_keys}",
+            "Not same quantity"
+            ], duration=2)
 
     for q_num, correct_answer in answer_key.items():
-        student_answer = student_answers.get(q_num, "missing_answer")
-        is_correct = student_answer == correct_answer
+        student_answer  = student_answers.get(q_num, "missing_answer")
+        
+        checking_result = student_answer == correct_answer
+        
+        if student_answer == "essay_answer":
+            is_final_score = False
+            breakdown[q_num] = {
+                "student_answer"    : student_answer,
+                "correct_answer"    : "will_check_by_teacher",
+                "checking_result"   : "pending",
+            }
+        else:
+            breakdown[q_num] = {
+                "student_answer"    : student_answer,
+                "correct_answer"    : correct_answer,
+                "checking_result"   : checking_result,
+            }
 
-        breakdown[q_num] = {
-            "student": student_answer,
-            "correct": correct_answer,
-            "is_correct": is_correct,
-        }
+            if checking_result:
+                score += 1
 
-        if is_correct:
-            score += 1
-
-    return score, total, breakdown
+    return score, total, breakdown, is_final_score
